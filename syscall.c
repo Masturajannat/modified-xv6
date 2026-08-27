715d0f27dc735360bb295c03f4f405a7154b0481
Masturajannat <masturanisha@gmail.com>
2026-08-27 23:19:42 +0600

    Clean diff report for MLFQ and fdinfo features

--- syscall.c
+++ syscall.c
@@ -103,7 +103,6 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int sys_hello(void);
 extern int sys_fdinfo(void);
 
 static int (*syscalls[])(void) = {
@@ -128,7 +127,6 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_hello] sys_hello,
 [SYS_fdinfo] sys_fdinfo,
 };
 
